e27f1e0ea9fce23e0a2569ebdbe16ed1bc293ba1
Doğu Boran Şentürk <dsenturk25@my.uaa.k12.tr>
2023-09-16 16:45:10 +0300

    working enough for a demo user recognition missing

--- sockets.js
+++ sockets.js
@@ -1,26 +1,29 @@
 
 const socketIo = require("socket.io");
 const { spawn } = require("child_process");
-const fs = require("fs");
 
 
 const PATH_NAME = "/realtime";
 const PREDICT_DIR = "../LedgeriseLens-AI/detect.py";
 const TEMP_IMAGE_DIR = "../Nft-Fundraising-nodejs-backend/temp_image.png"
 
-const processImage = async (imageBase64) => {
-  const pythonProcess = spawn("python3", [PREDICT_DIR, TEMP_IMAGE_DIR]);
+const processImage = (imageBase64) => {
+  return new Promise((resolve, reject) => {
+    const pythonProcess = spawn("python3", [PREDICT_DIR]);
 
-  pythonProcess.stdout.on("data", (data) => {
-    const processedImage = data.toString().trim();
-    return processedImage
-  })
+    pythonProcess.stdin.write(imageBase64);
+    pythonProcess.stdin.end();
 
-  pythonProcess.stderr.on("data", (data) => {
-    const processedImage = data.toString().trim();
-    return processedImage
-  })
+    pythonProcess.stdout.on("data", (data) => {
+      const processedImage = data.toString().trim();
+      resolve(processedImage)
+    })
 
+    pythonProcess.stderr.on("data", (data) => {
+      const processedImage = data.toString().trim();
+      reject(processedImage);
+    })
+  });
 }
 
 let tempBase64Image = ""
@@ -38,15 +41,13 @@ const connectRealTime = (server) => {
       if (base64ImageData != "done") {
         tempBase64Image += base64ImageData;
       } else if (base64ImageData == "done") {
-        fs.writeFileSync('temp_image.png', Buffer.from(tempBase64Image, "base64"));
 
         const processedImageData = await processImage(tempBase64Image);
-        console.log(processedImageData)
-        if (processedImageData == "done") {
-          console.log("hello")
-          socket.emit("processedImage", processedImageData.toString().trim());
-          tempBase64Image = "";
-
+        tempBase64Image = "";
+        if (processedImageData != undefined) {
+          const formattedProcessedImageData = processedImageData.replace(/'/g, '"')
+          console.log(formattedProcessedImageData);
+          await socket.emit('processedImage', JSON.parse(formattedProcessedImageData));
         }
       }
     })
