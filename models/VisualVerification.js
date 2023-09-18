35a9c1cc1bc7c0ecc6e179cfbc98bebaa32aad18
Doğu Boran Şentürk <dsenturk25@my.uaa.k12.tr>
2023-09-18 07:57:23 +0300

    added visual verifications

---
+++ models/VisualVerification.js
@@ -0,0 +1,48 @@
+
+const mongoose = require("mongoose");
+
+const visualVerificationSchema = new mongoose.Schema({
+  buyer: {
+    type: String,
+    default: "0x",
+    required: true
+  },
+  key: {  // stamp, shipped, delivered
+    type: String,
+    default: "nokey",
+    required: true
+  },
+  openseaTokenId: {
+    type: String,
+    required: true
+  },
+  tokenId: {
+    type: Number,
+    required: true
+  },
+  base64_image: {
+    type: String,
+    default: ""
+  },
+  tokenUri: {
+    type: String,
+    default: ""
+  },
+  isUploadedToBlockchain: {
+    type: Boolean,
+    default: false
+  }
+});
+
+visualVerificationSchema.statics.createVisualVerification = function (body, callback) {
+  const newVisualVerification = new visualVerification(body);
+  if (newVisualVerification) {
+    newVisualVerification.save();
+    return callback(null, newVisualVerification);
+  }
+  return callback("bad_request");
+}
+
+const visualVerification = mongoose.model("visualverification", visualVerificationSchema);
+
+module.exports = visualVerification
