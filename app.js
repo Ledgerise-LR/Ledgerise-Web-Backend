3d46a3658065b9f7c3247504e1e556002311574c
Doğu Boran Şentürk <dsenturk25@my.uaa.k12.tr>
2024-03-06 12:51:26 +0300

    bug fix

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', `${FRONTEND_URL}:${FRONTEND_PORT}`); // Replace with your Next.js domain
+  res.header('Access-Control-Allow-Origin', `*`); // Replace with your Next.js domain
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
