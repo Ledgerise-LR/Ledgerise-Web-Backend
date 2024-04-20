b22f83bc77a6d6502acdfb92b803ab8fee8be890
Doğu Boran Şentürk <dsenturk25@my.uaa.k12.tr>
2024-04-20 14:27:16 +0300

    bug fix

--- controllers/company/get/getAllCollectionsOfCompanyGetController.js
+++ controllers/company/get/getAllCollectionsOfCompanyGetController.js
@@ -3,14 +3,16 @@ const Company = require("../../../models/Company");
 
 module.exports = (req, res) => {
 
-  const body = {};
+  const body = {
+    code: ""
+  };
 
   if (req.query.code) {
     body.code = req.query.code;
   } else if (req.body.code) {
     body.code = req.body.code;
   } else if (req.session.company) {
-    body = req.session.company
+    body.code = req.session.company.code
   }
 
   Company.getAllCollectionsOfCompany(body, (err, subcollections) => {
