8a14d1952fe54b482fd2a00b1069644a3bcfe071
Doğu Boran Şentürk <dsenturk25@my.uaa.k12.tr>
2024-06-18 15:02:28 +0300

    .

--- controllers/auth/get/authenticateVerifierGetController.js
+++ controllers/auth/get/authenticateVerifierGetController.js
@@ -2,6 +2,8 @@
 const Company = require("../../../models/Company");
 
 module.exports = (req, res) => {
+  console.log(req.session.company);
+  console.log(req.query);
   if (req.session.company != null || req.query.code != null) {
     Company.authenticateVerifier(req.query || req.session.company, (err, company) => {
       if (err || !company) return res.json({ success: false, err: err });
