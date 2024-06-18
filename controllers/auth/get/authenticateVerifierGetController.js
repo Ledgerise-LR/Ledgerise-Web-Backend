3c27cd884e972db9acf2fbf3f76655acdce99494
Doğu Boran Şentürk <dsenturk25@my.uaa.k12.tr>
2024-06-18 14:56:57 +0300

    bug fix

--- controllers/auth/get/authenticateVerifierGetController.js
+++ controllers/auth/get/authenticateVerifierGetController.js
@@ -2,8 +2,8 @@
 const Company = require("../../../models/Company");
 
 module.exports = (req, res) => {
-  if (req.session.company != null) {
-    Company.authenticateVerifier(req.session.company, (err, company) => {
+  if (req.session.company != null || req.query.code != null) {
+    Company.authenticateVerifier(req.query || req.session.company, (err, company) => {
       if (err || !company) return res.json({ success: false, err: err });
       return res.status(200).json({ success: true, company: company });
     })
